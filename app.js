4d358b749ce6269758a5142428ee85fccb1d854d
LandenTy <93301002+LandenTy@users.noreply.github.com>
2023-08-28 10:43:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,39 @@ $(document).ready(function(){
       }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectButtons = document.querySelectorAll(".project-button");
+  const overlays = document.querySelectorAll(".overlay");
+
+  projectButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      // Close all open overlays
+      overlays.forEach((overlay) => {
+        overlay.style.display = "none";
+      });
+
+      // Open the selected overlay
+      const projectId = button.getAttribute("data-project");
+      const overlay = document.getElementById(`${projectId}-overlay`);
+      overlay.style.display = "block";
+    });
+  });
+
+  overlays.forEach((overlay) => {
+    const closeButton = overlay.querySelector(".close-button");
+    closeButton.addEventListener("click", () => {
+      overlay.style.display = "none";
+    });
+
+    const carousel = overlay.querySelector(".carousel");
+    const images = carousel.querySelectorAll("img");
+    let currentIndex = 0;
+
+    setInterval(() => {
+      images[currentIndex].style.transform = "translateX(-100%)";
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].style.transform = "translateX(0)";
+    }, 3000);
+  });
+});
